ece444447d7003a7b099f8871fb150fef62ad8f4
SahilKumarGit <email.sahil@yahoo.com>
2023-01-10 15:13:52 +0530

    feat: good

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-export * as br from './falticons/br';
-export * as bs from './falticons/bs';
-export * as rr from './falticons/rr';
-export * as rs from './falticons/rs';
-export * as sr from './falticons/sr';
-export * as ss from './falticons/ss';
+export * from './falticons/br';
+export * from './falticons/bs';
+export * from './falticons/rr';
+export * from './falticons/rs';
+export * from './falticons/sr';
+export * from './falticons/ss';
